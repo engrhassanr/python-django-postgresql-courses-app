968095d2c876b7c2212d78900be4409a06d7ca91
Hassan Raza <engrhassanr@gmail.com>
2025-02-12 16:55:36 +0500

    Initial Railway deployment

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -84,8 +84,10 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 
+
+
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    "default": dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
 
 # Password validation
