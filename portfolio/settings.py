dc6ddfac5cca191b8118a8f06ec35ef8069e13c9
Hassan Raza <engrhassanr@gmail.com>
2025-02-13 14:56:19 +0500

    Fix CSRF issue by adding trusted origins

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -17,6 +17,9 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://python-django-postgresql-courses-django-python.up.railway.app",
+]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -27,7 +30,8 @@ SECRET_KEY = 'django-insecure-7v*1%@)4myixsh(eebj5yg11c#^#r55s*9a-5ynfo34_)dfj2@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["python-django-postgresql-courses-django-python.up.railway.app"]
+
 
 
 # Application definition
