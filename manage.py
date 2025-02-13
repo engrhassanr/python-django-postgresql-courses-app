40759fcc07207c9862e3b80bcf0df0a45c8e93a1
Hassan Raza <engrhassanr@gmail.com>
2025-02-13 11:28:00 +0500

    Fix Railway deployment

--- manage.py
+++ manage.py
@@ -17,9 +17,7 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
-
-if __name__ == '__main__':
-   if __name__ == "__main__":
+if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portfolio.settings")
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
