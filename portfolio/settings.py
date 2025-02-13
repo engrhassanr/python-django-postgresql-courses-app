7bf3f35a36200b09e571ce67b9a11eb7853a3a4a
Hassan Raza <engrhassanr@gmail.com>
2025-02-13 11:23:21 +0500

    Fix Railway deployment

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7v*1%@)4myixsh(eebj5yg11c#^#r55s*9a-5ynfo34_)dfj2@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
